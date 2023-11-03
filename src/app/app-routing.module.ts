2ce17d34e76878cb529925890af16cc1f35911ca
ShaluRajput9951 <shalurajput9951@gmail.com>
2023-11-03 23:58:29 +0530

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TutorialsListComponent } from './components/tutorial-list/tutorial-list.component';
+import { TutorialDetailsComponent } from './components/tutorial-details/tutorial-details.component';
+import { AddTutorialComponent } from './components/add-tutorial/add-tutorial.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '' , redirectTo: 'turorials' , pathMatch: 'full'},
+  {
+    path: 'tutorials' , component: TutorialsListComponent
+  },
+  { path : 'tutorials/:id' , component: TutorialDetailsComponent},
+  { path:'add' , component: AddTutorialComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
